b75009deca7b2605a4fde155714df445e1f7003b
Grewer <grewer@grewer.cn>
2018-06-05 14:44:57 +0800

    feat(location): 同一vw规定

--- config/webpack.config.dev.js
+++ config/webpack.config.dev.js
@@ -11,12 +11,9 @@ const eslintFormatter = require('react-dev-utils/eslintFormatter');
 const ModuleScopePlugin = require('react-dev-utils/ModuleScopePlugin');
 const getClientEnvironment = require('./env');
 const paths = require('./paths');
-const postcssAspectRatioMini = require('postcss-aspect-ratio-mini');
-const postcssPxToViewport = require('postcss-px-to-viewport');
-const postcssWriteSvg = require('postcss-write-svg');
-const postcssCssnext = require('postcss-cssnext');
-const postcssViewportUnits = require('postcss-viewport-units');
-const cssnano = require('cssnano');
+const vwConfig = require('./vw.config')
+
+
 // Webpack uses `publicPath` to determine where the app is being served from.
 // In development, we always serve from the root. This makes config easier.
 const publicPath = '/';
@@ -188,26 +185,7 @@ module.exports = {
                       ],
                       flexbox: 'no-2009',
                     }),
-                    postcssAspectRatioMini({}),
-                    postcssPxToViewport({
-                      viewportWidth: 750, // (Number) The width of the viewport.
-                      viewportHeight: 1334, // (Number) The height of the viewport.
-                      unitPrecision: 3, // (Number) The decimal numbers to allow the REM units to grow to.
-                      viewportUnit: 'vw', // (String) Expected units.
-                      selectorBlackList: ['.ignore', '.hairlines'], // (Array) The selectors to ignore and leave as px.
-                      minPixelValue: 1, // (Number) Set the minimum pixel value to replace.
-                      mediaQuery: false // (Boolean) Allow px to be converted in media queries.
-                    }),
-                    postcssWriteSvg({
-                      utf8: false
-                    }),
-                    postcssCssnext({}),
-                    postcssViewportUnits({}),
-                    cssnano({
-                      preset: "advanced",
-                      autoprefixer: false,
-                      "postcss-zindex": false
-                    })
+                    ...vwConfig
                   ],
                 },
               },
--- src/App.js
+++ src/App.js
@@ -10,6 +10,9 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
+        <div className="myBorder">
+          内容
+        </div>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
